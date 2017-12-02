0912b38e160c0a99f863c9fbcb75546c8cde9001
cope1010 <jakekcope@gmail.com>
2017-12-02 17:01:46 -0500

    added the beginning of the ViewMediator class, more to add later.

---
+++ Final_Project/src/projectview/ViewMediator.java
@@ -0,0 +1,27 @@
+package projectview;
+
+import java.util.Observable;
+import javax.swing.JFrame;
+import project.MachineModel;
+
+public class ViewMediator extends Observable {
+	
+	private MachineModel model;
+	private JFrame frame;
+	
+	public void step() {
+		
+	}
+	
+	public MachineModel getModel() {
+		return model;
+	}
+	
+	public void setModel(MachineModel model) {
+		this.model = model;
+	}
+	
+	public JFrame getFrame() {
+		return frame;
+	}
+}
